8d269a3c3cc0f99403e7e2f35077b6d6cc9d149c
wkaanig <15670813638@163.com>
2021-07-21 17:03:12 +0800

    新增爬虫url的redis bloomfilter去重

--- backend/template/scrapy/config_spider/spiders/spider.py
+++ backend/template/scrapy/config_spider/spiders/spider.py
@@ -17,6 +17,17 @@ class ConfigSpider(scrapy.Spider):
     source = '###SOURCES###'
     crawler_name = '###CRAWLER_NAME###'
 
+    @classmethod
+    def update_settings(cls, settings):
+        if settings.get("check_url"):
+            # bloomfilter
+            settings.setdict(cls.custom_settings or {
+                "SCHEDULER": "scrapy_redis_bloomfilter.scheduler.Scheduler",
+                "SCHEDULER_DUPEFILTER_KEY":"crawlab_spider:bloomfilter",
+                "DUPEFILTER_CLASS":"scrapy_redis_bloomfilter.dupefilter.RFPDupeFilter"
+                "DUPEFILTER_DEBUG": True
+            }, priority='spider')
+
     def start_requests(self):
         yield scrapy.Request(url='###START_URL###', callback=self.###START_STAGE###)
 
